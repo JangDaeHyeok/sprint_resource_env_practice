7f1a01d93f1a5642e3a01d802cd0f6897bb54b6d
JangDaeHyeok <jangdaehyeog@gmail.com>
2022-10-25 22:59:04 +0900

    project init

---
+++ resourceEnvTest/src/main/java/com/jdh/resourceEnvTest/ResourceEnvTestApplication.java
@@ -0,0 +1,13 @@
+package com.jdh.resourceEnvTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceEnvTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceEnvTestApplication.class, args);
+	}
+
+}
---
+++ resourceEnvTest/src/test/java/com/jdh/resourceEnvTest/ResourceEnvTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdh.resourceEnvTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceEnvTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
